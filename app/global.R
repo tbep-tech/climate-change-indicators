8a1c6b0d394bc4d46c4bf1ff93e20e1a993f6270
Ben Best <ben@ecoquants.com>
2024-05-24 16:02:39 +0200

    TODOs

--- app/global.R
+++ app/global.R
@@ -1,15 +1,18 @@
 # TODO:
 # - [ ] style ggplotly https://rstudio.github.io/thematic/#shiny
+#          https://rstudio.github.io/thematic/articles/auto.html
+# - [ ] icons for Map | Plot
 # - [ ] show sources for prism, sst, etc with About and links per card
 # - [ ] convert prism units: ºC -> ºF, mm -> in
 # - [ ] sel_t_var implement in map_prism_temp w/ avg
 # - [ ] swiper maps: precipitation, ocean temperature
 # - [ ] temp: heat index using dewpoint (`tdmean` -> humidity)
 # - [ ] prism: show `version`, `date_updated`
-# - [ ] swiper maps: leaflet-proxy-map updates to layers so can zoom / pan without refresh
 # - [ ] time series cards / panels
+# - [ ] create static figures for reports (auto parameterize rendered)
+# - [x] swiper maps: leaflet-proxy-map updates to layers so can zoom / pan without refresh
 
-# devtools::install_local(here("../tbeptools"), force = T)
+# devtools::install_local(here::here("../tbeptools"), force = T)
 # devtools::load_all(here::here("../tbeptools"))
 librarian::shelf(
   bsicons, bslib, dplyr, glue, here, leaflet, leaflet.extras2, lubridate, plotly,
